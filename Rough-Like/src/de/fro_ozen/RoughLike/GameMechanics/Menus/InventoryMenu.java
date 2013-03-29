638b4a137367228aead31cdea29fe3033c20fa8d
froozen <froozen@froozen-laptop>
2013-03-29 06:44:20 +0100

    A little change

--- Rough-Like/src/de/fro_ozen/RoughLike/GameMechanics/Menus/InventoryMenu.java
+++ Rough-Like/src/de/fro_ozen/RoughLike/GameMechanics/Menus/InventoryMenu.java
@@ -16,32 +16,32 @@ public class InventoryMenu extends GMenu{
 	private GItemArea inventory;
 	private GTextArea descriptions;
 	private GEquipmentArea equipment;
-	
+
 	public InventoryMenu(Player player){
 		triggerKey = "inventory";
-		
+
 		setBounds(100,105,600,390);
-		
+
 		descriptions = new GTextArea();
 		descriptions.setBounds(425, 200, 165, 180);
-		
+
 		inventory = new GItemArea();
 		inventory.setDisplayItems(GameLoop.player.invent.content);
 		inventory.setBounds(10, 10, 405, 380);
 		inventory.setItemFieldListener(new InventoryFieldHandler());
 		inventory.setDescriptionArea(descriptions);
-		
+
 		equipment = new GEquipmentArea();
 		equipment.setBounds(425, 10, 0, 0);
 		equipment.setDescriptionArea(descriptions);
 		equipment.setItemFieldListener(new EquipmentFieldHandler());
-		
+
 		add(equipment);
 		add(descriptions);
 		add(inventory);
 		setShift();
 	}
-	
+
 	private class InventoryFieldHandler extends GItemFieldListener{
 		public void activate(GItemField source) {
 			if(source.display != null)source.display.use(GameLoop.player);
