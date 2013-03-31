13efc16355a761e46d7d19c9ce40bc3a8627dfad
froozen <froozen@froozen-laptop>
2013-03-31 14:08:46 +0200

    Redid the HUD

--- Rough-Like/src/de/fro_ozen/RoughLike/BaseTypes/Misc/Leveling.java
+++ Rough-Like/src/de/fro_ozen/RoughLike/BaseTypes/Misc/Leveling.java
@@ -1,12 +1,16 @@
 package de.fro_ozen.RoughLike.BaseTypes.Misc;
 
+import de.fro_ozen.RoughLike.BaseTypes.Simple.VariablePair;
+
 public class Leveling {
-	public int Level, expToNext, expEver, expMoment;
+	public int Level, expEver;
+	public VariablePair exp;
 	public Leveling(){
 		Level = 1;
-		expToNext = 25;
+		exp = new VariablePair();
+		exp.max = 25;
 		expEver = 0;
-		expMoment = 0;
+		exp.real = 0;
 	}
 	private void levelUp(){
 		Level++;
@@ -14,15 +18,15 @@ public class Leveling {
 	public void addExp(int amount){
 		expEver+=amount;
 		while(amount>0){
-			if((expToNext-expMoment)-amount<=0){
+			if((exp.max-exp.real)-amount<=0){
 				levelUp();
 				System.out.println("Level Up!");
-				amount=amount-(expToNext-expMoment);
-				expToNext = 2*expToNext;
-				expMoment = 0;
+				amount=amount-(exp.max-exp.real);
+				exp.max = 2*exp.max;
+				exp.real = 0;
 			}
 			else{
-				expMoment+=amount;
+				exp.real+=amount;
 				amount=0;
 			}
 		}
--- Rough-Like/src/de/fro_ozen/RoughLike/GameMechanics/HUD.java
+++ Rough-Like/src/de/fro_ozen/RoughLike/GameMechanics/HUD.java
@@ -3,41 +3,28 @@ package de.fro_ozen.RoughLike.GameMechanics;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
-import de.fro_ozen.GUI.elements.GButton;
-import de.fro_ozen.GUI.elements.GButtonListener;
-import de.fro_ozen.GUI.elements.GMenu;
 import de.fro_ozen.RoughLike.BaseTypes.Entitys.Player;
-import de.fro_ozen.RoughLike.BaseTypes.Items.ArmorItem;
-import de.fro_ozen.RoughLike.BaseTypes.Items.BaseItem;
-import de.fro_ozen.RoughLike.BaseTypes.Items.BaseWeapon;
-import de.fro_ozen.RoughLike.BaseTypes.Items.ItemStack;
 
 public class HUD{
-	GButton test;
-	final int xrow = 14, yrow = 14;
-	BaseItem preview;
 	private Player display;
 	final int barLength = 200;
-	private double HPbarLength;
-	private double MPbarLength;
-	private BufferedImage hpbar, mpbar, nonebar, levelelement, background, choiceIndicator;
+	private int expBarSize;
+	private double HPbarLength, MPbarLength, expBarHeight;
+	private BufferedImage hpbar, mpbar, expbar, overlay, underlay;
 	public HUD(Player p){
 		display = p;
 		try {
-			nonebar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/No-Bar.png"));
-			levelelement = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/Level.png"));
-			background = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/background.png"));
-			choiceIndicator = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/choiceindicator.png"));
-		} catch (IOException e) {
+			underlay = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/HUD-underlay.png"));
+			overlay = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/HUD-overlay.png"));
+			expbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/expbar.png"));
+			expBarSize = expbar.getHeight();
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -45,28 +32,31 @@ public class HUD{
 	private void displayBaseHUD(Graphics g){
 		HPbarLength = ((double)display.stats.hp.real/display.stats.hp.max)*barLength;
 		MPbarLength= ((double)display.stats.mp.real/display.stats.mp.max)*barLength;
+		expBarHeight = ((double)display.levels.exp.real/display.levels.exp.max)*expBarSize;
 		try {
-			nonebar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/No-Bar.png"));
-			hpbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/HP-Bar.png"));
-			mpbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/MP-Bar.png"));
+			hpbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/hpbar.png"));
+			mpbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/mpbar.png"));
+			expbar = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Sprites/HUD/expbar.png"));
 			if(HPbarLength <= 0)HPbarLength = 1;
 			if(MPbarLength <= 0)MPbarLength = 1;
-			hpbar = hpbar.getSubimage(0, 0, (int)HPbarLength, 20);
-			mpbar = mpbar.getSubimage(0, 0, (int) MPbarLength, 20);
+			if(expBarHeight <= 0)expBarHeight = 1;
+			hpbar = hpbar.getSubimage(0, 0, (int)HPbarLength, 10);
+			mpbar = mpbar.getSubimage(0, 0, (int) MPbarLength, 10);
+			expbar = expbar.getSubimage(0, 0, 8, (int)expBarHeight);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		g.drawImage(nonebar, 50, 5, null);
-		g.drawImage(nonebar, 18, 27, null);
-		g.drawImage(hpbar, 50, 5, null);
-		g.drawImage(mpbar, 18, 27, null);
-		g.drawImage(levelelement,-5,-5, null);
+		g.drawImage(underlay, 0, 0, null);
+		g.drawImage(hpbar, 80, 2, null);
+		g.drawImage(mpbar, 80, 17, null);
+		g.drawImage(expbar, 37, 38-expbar.getHeight(), null);
+		g.drawImage(overlay, 0, 0, null);
 		g.setColor(Color.white);
-		g.drawString("MP:       "+(int)display.stats.mp.real+"/"+display.stats.mp.max, 57, 42);
-		g.drawString("HP:       "+(int)display.stats.hp.real+"/"+display.stats.hp.max, 60, 20);
-		if(display.equip.mainHand != null)g.drawImage(display.equip.mainHand.icon, 7, 7, null);
-		g.setFont(g.getFont().deriveFont(Font.PLAIN, 18));
-		g.drawString(""+display.levels.Level, 20, 15);
+		g.setFont(g.getFont().deriveFont(Font.PLAIN, 10));
+		g.drawString(""+display.stats.hp.real, 48, 10);
+		g.drawString(""+display.stats.mp.real, 48, 25);
+		if(display.equip.mainHand != null)g.drawImage(display.equip.mainHand.icon, 8, 7, null);
+		g.drawString(""+display.levels.Level, 5, 41);
 	}
 
 	public void displayHUD(Graphics g){
